42bdf59f2fbf20323060fc4d84f65fc70c4c63ea
isaque hipolito marion silva <qISAQUEp@gmail.com>
2025-05-12 21:47:27 -0300

    aula12

---
+++ HTML/aula12/js/script.js
@@ -0,0 +1,9 @@
+var container = document.querySelector('.section');
+var spinner = document.querySelector('.spinner');
+
+container.classList.add('load');
+spinner.classList.add('spinner');
+
+var interval = setInterval(function() {
+
+}, 1000);
