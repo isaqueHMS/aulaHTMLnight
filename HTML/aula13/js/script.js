2bbb0b75d07f816929d80a5c9b5ad19108c07e32
isaque hipolito marion silva <qISAQUEp@gmail.com>
2025-05-14 21:45:02 -0300

    aula 13 Grand Theft Auto IV

---
+++ HTML/aula13/js/script.js
@@ -0,0 +1,9 @@
+//laços de  repetições
+let contador = 0
+
+
+while (contador <= 1000) {
+        console.log("O número " + contador + " é par")
+    contador = contador + 2
+}
+console.log("fim do processo")
