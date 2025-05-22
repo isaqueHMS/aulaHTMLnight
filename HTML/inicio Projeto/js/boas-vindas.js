863f9c6fb0c10956a4c0b59feee6a51e0f12e268
isaque hipolito marion silva <qISAQUEp@gmail.com>
2025-05-21 21:44:20 -0300

    projeto-aula15

--- HTML/inicio Projeto/js/boas-vindas.js
+++ HTML/inicio Projeto/js/boas-vindas.js
@@ -8,8 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const usuario = JSON.parse(usuarioJSON);
 
             // Atualiza o cabeçalho com o nome do usuário
-            const nomeDiv = document.getElementById('usuario-nome');
-            if (nomeDiv) nomeDiv.textContent = `Olá, ${usuario.nome}`;
+           
 
             // Exibe a tela de boas-vindas se ainda não tiver sido exibida
             if (!sessionStorage.getItem('boasVindasExibida')) {
